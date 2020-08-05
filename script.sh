0f62c127ff5ea299cebd472ba4c297eee3cbc35d
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2020-08-06 03:50:43 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,11 +10,5 @@ if [[ ! ($filename && $ext && $origext) ]]; then
     exit 1
 fi
 
-ls -1 | sort -V
-read -p "you want to concat movie file in this directory? [yes/no]" result
-if [[ $result != "yes" ]]; then
-    exit 1
-fi
-
 ls -1 | sort -V | sed 's/ /\\ /g' |awk '{print "file " $0}' | ffmpeg -f concat -protocol_whitelist file,pipe -safe 0 -i - -c copy  "${filename}.${origext}"
 ffmpeg -i "${filename}.${origext}" "${filename}.${ext}"
